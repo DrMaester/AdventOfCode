28cbe200f3c95babe82d5a817f8825ba537a2272
siemensdaniel <siemensdaniel@outlook.de>
2019-12-19 13:00:09 +0100

    day 4

--- 2019/04.py
+++ 2019/04.py
@@ -1,16 +1,60 @@
-def getLines(path):
-    with open(path, 'r') as f:
-        return [line.strip() for line in f]
+import re
+
+def isNeverDecreasing(number):
+    lastNumber = 0
+    for digit in str(number):
+        if int(digit) < int(lastNumber):
+            return False
+        lastNumber = digit
+
+    return True
+
+def hasTwoAdjacentSameNumbers(number):
+    lastNumber = 0
+    for digit in str(number):
+        if int(digit) == int(lastNumber):
+            return True
+        lastNumber = digit
+
+    return False
+    
+def hasTwoAdjacentSameNumbers2(number):
+    num = str(number)
+    matches = re.findall('00+|11+|22+|33+|44+|55+|66+|77+|88+|99+', num)
+    if matches and min([len(match) for match in matches])==2:
+        return True
+    else:
+        return False
+
+def meetsCriteria(number):
+    if isNeverDecreasing(number) and hasTwoAdjacentSameNumbers(number):
+        return True
+    return False
+
+def meetsCriteria2(number):
+    if isNeverDecreasing(number) and hasTwoAdjacentSameNumbers2(number):
+        return True
+    return False
 
 def firstTask():
-    lines = getLines('Sources/04_01.txt')
-    
-    pass
+    min = 353096
+    max = 843212
+    count = 0
+    for number in range(min, max + 1):
+        if meetsCriteria(number):
+            count += 1
+
+    print(count)
 
 def secondTask():
-    lines = getLines('Sources/04_02.txt')
-    
-    pass
+    min = 353096
+    max = 843212
+    count = 0
+    for number in range(min, max + 1):
+        if meetsCriteria2(number):
+            count += 1
+
+    print(count)
 
 # firstTask()
-# secondTask()
+secondTask()
